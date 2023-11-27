3168e7245fe90203232259daa6afc6471e362149
Zachary Blickensderfer <zach.blickensderfer@gmail.com>
2023-11-27 13:46:04 -0800

    Created new constructor for Card class.

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Card {
+public class Card {
+
+    private int point;
+    private String suit;
+    private String rank;
+
+    public Card(String suit) {
+        this.suit = suit;
+    }
 }
