1434fcfc53d4bde5c476cdd5ed2b2734ce09d7db
Zachary Blickensderfer <zach.blickensderfer@gmail.com>
2023-11-27 13:47:36 -0800

    Edited constructor for Card class.

--- src/Card.java
+++ src/Card.java
@@ -4,7 +4,8 @@ public class Card {
     private String suit;
     private String rank;
 
-    public Card(String suit) {
+    public Card(String suit, String rank) {
         this.suit = suit;
+        this.rank = rank;
     }
 }
